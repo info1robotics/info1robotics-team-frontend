ba96ac70e3fc3753ec2fe0d38e98dd48f56a0be1
Alex Sandulescu <alexandru.prahova@gmail.com>
2020-12-12 15:27:12 +0200

    Updated branding to be team-neutral

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
-import React, {useContext} from 'react';
-import {Link, withRouter} from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import UsersService from '../services/UsersService';
 import { AuthContext } from '../contexts/AuthContext';
 
 
 const Navbar = props => {
-    const { isAuthenticated, user, setIsAuthenticated, setUser} = useContext(AuthContext);
+    const { isAuthenticated, user, setIsAuthenticated, setUser } = useContext(AuthContext);
 
     const onClickLogoutHandler = () => {
         UsersService.logout().then(data => {
-            if(data.success) {
+            if (data.success) {
                 setUser(data.user);
                 setIsAuthenticated(false);
                 props.history.push('/home');
@@ -35,31 +35,31 @@ const Navbar = props => {
 
     const authenticatedNav = () => {
         return (
-        <>
-            <Link to="/">
-                <li className="btn btn-link nav-item nav-link">Home</li>
-            </Link>
-            <Link to="/uploads">
-                <li className="btn btn-link nav-item nav-link">Uploads</li>
-            </Link>
-            {
-                user.role === "admin" ?
-                <Link to="/admin">
-                    <li className="btn btn-link nav-item nav-link">Admin</li>
-                </Link> : null
-            }
-            <Link to="/visits">
-                <li className="btn btn-link nav-item nav-link">Lab Visits</li>
-            </Link>
-            <button type="button"
-                className="btn btn-link nav-item nav-link" onClick={onClickLogoutHandler}>Logout</button>
-            
-        </>
+            <>
+                <Link to="/">
+                    <li className="btn btn-link nav-item nav-link">Home</li>
+                </Link>
+                <Link to="/uploads">
+                    <li className="btn btn-link nav-item nav-link">Uploads</li>
+                </Link>
+                {
+                    user.role === "admin" ?
+                        <Link to="/admin">
+                            <li className="btn btn-link nav-item nav-link">Admin</li>
+                        </Link> : null
+                }
+                <Link to="/visits">
+                    <li className="btn btn-link nav-item nav-link">Lab Visits</li>
+                </Link>
+                <button type="button"
+                    className="btn btn-link nav-item nav-link" onClick={onClickLogoutHandler}>Logout</button>
+
+            </>
         );
     }
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light shadow rounded mb-5">
-            <div className="brand-name">infO(1) Robotics</div>
+            <div className="brand-name">QuickDash</div>
             <Link to="/">
                 <div className="navbar-brand"></div>
             </Link>
@@ -68,11 +68,11 @@ const Navbar = props => {
             </button>
             <div className="collapse navbar-collapse" id="navbarText">
                 <ul className="navbar-nav mr-auto">
-                    {!isAuthenticated? unauthenticatedNav() : authenticatedNav()}
+                    {!isAuthenticated ? unauthenticatedNav() : authenticatedNav()}
                 </ul>
             </div>
-            <span className={`navbar-text font-weight-bold${user.username === "salexandru"? " text-danger" : ""}`}>
-                { isAuthenticated? user.username : "Guest"}
+            <span className={`navbar-text font-weight-bold${user.username === "salexandru" ? " text-danger" : ""}`}>
+                {isAuthenticated ? user.username : "Guest"}
             </span>
         </nav>
     );
